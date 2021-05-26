9a27b702c1c34c811ceb7d8bcbca7491a9d26d23
pton@ascmt.com <pton@ascmt.com>
2021-05-26 08:32:43 -0700

    Changed to use production database

--- ASC Coil Tracker Production/Data Access Layer/CoilContext.cs
+++ ASC Coil Tracker Production/Data Access Layer/CoilContext.cs
@@ -12,7 +12,7 @@ namespace ASC_Coil_Tracker_Production.Data_Access_Layer
         private const string PROD = "name=JOBSHEETSPOCK";
 
         public CoilContext()
-            : base(TEST)
+            : base(PROD)
         {
         }
 
