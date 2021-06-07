05788b9ac897c3e0db0f6b3b2e72db535ba98ca8
pton@ascmt.com <pton@ascmt.com>
2021-06-07 08:34:38 -0700

    Improved search field performance

--- ASC Coil Tracker Production/Scripts/helpers/OnFilterChange.js
+++ ASC Coil Tracker Production/Scripts/helpers/OnFilterChange.js
@@ -1,4 +1,6 @@
-﻿// Update view whenever length filter changed (non-depleted/all)
+﻿var changed = false;
+
+// Update view whenever length filter changed (non-depleted/all)
 $('#lengthFilter').on('change', function () {
     var form = $(this).parents('form');
 
@@ -7,14 +9,24 @@ $('#lengthFilter').on('change', function () {
 
 // Update view whenever search field changed
 $('#searchString').on('input', function () {
-    var form = $(this).parents('form');
-
-    form.submit();
+    changed = true;
 });
 
-// Refresh view with results from search field
-function refresh(result) {
-    $("body").html(result);
-    $('#searchString').focus();
-    $('#searchString')[0].setSelectionRange(1000, 1000);
-}
+// Watch for changes to search field
+setInterval(function () {
+    if (changed) {
+        var form = $('#searchString').parents('form');
+
+        $.ajax({
+            type: "GET",
+            data: form.serialize(),
+            success: function (data) {
+                $("body").html(data);
+                $('#searchString').focus();
+                $('#searchString')[0].setSelectionRange(1000, 1000);
+            }
+        });
+
+        changed = false;
+    }
+}, 1);
