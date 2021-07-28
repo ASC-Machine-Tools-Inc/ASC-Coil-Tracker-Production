11c0eb6e01731e1b3a50c3d38fd8af9ebea097b1
Phi <pton@ascmt.com>
2021-07-28 13:09:51 -0700

    PDF export functionality finished for coils

--- ASC Coil Tracker Production/Scripts/helpers/HtmlToPdf.js
+++ ASC Coil Tracker Production/Scripts/helpers/HtmlToPdf.js
@@ -1,4 +1,7 @@
-﻿const MAX_NOTE_LENGTH = 15;
+﻿// Constants for the notes table column length
+const TAG_OPENING_LENGTH = 18;
+const TAG_CLOSING_LENGTH = 5;
+const MAX_NOTE_LENGTH = 15;
 
 function setHtml() {
     // Get CSS
@@ -8,23 +11,26 @@ function setHtml() {
         "<link href='/Content/print.css' rel='stylesheet'> </head>";
 
     // Get HTML
-    let html = "<body><div id='print-table'>" + $("#coilTable")[0].outerHTML + "</div></body>";
+    let html = "<body><div id='print-table'>" + $("#coilTable")[0].innerHTML + "</div></body>";
 
     // Add table-sm class to the table.
     html = html.replace("table m-0", "table table-sm m-0");
 
     // Truncate notes if they're too long.
-    /*
-    html = html.match(/<td class="clamp">(.*?)<\/td>/g).map(function (note) {
-        if (note.length > MAX_NOTE_LENGTH) {
-            return note.substring(0, MAX_NOTE_LENGTH - 3) + "...";
-        } else {
-            return note;
-        }
-    });
-    */
+    let notesRegex = /<td class="clamp">((.|\n)*?)<\/td>/g;
+    html = html.replace(notesRegex, function (note) {
+        // Strip the tags and whitespace from the note.
+        let strippedNote = note.substring(TAG_OPENING_LENGTH,
+            note.length - TAG_CLOSING_LENGTH).trim();
 
-    console.log(html);
+        if (strippedNote.length > MAX_NOTE_LENGTH) {
+            // Truncate and re-add the tags.
+            strippedNote = strippedNote.substring(0, 25).trim();
+            note = "<td class='clamp'>" + strippedNote + "...</td>";
+        }
+
+        return note;
+    });
 
     // Load into hidden input value.
     $("#tableHtml").val("<html>" + css + html + "</html>");
